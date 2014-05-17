93346ab4fdae6e58f1672923e3546fb6cc1b5c52
Riad Djemili <email@riad.de>
2014-05-17 23:10:53 +0200

    toggle dates

--- goodhabits.js
+++ goodhabits.js
@@ -1,8 +1,14 @@
 var habits = [];
 
+getHabit = function(id) {
+  var habit = habits.find(function(h){return h.id == id})
+  assert(habit)
+  return habit;
+}
+
 addHabit = function(name) {
   newHabit = {
-    id: habits.length,
+    id: "habit-"+habits.length,
     name: name,
     history: []
   }
@@ -22,10 +28,65 @@ trashHabit = function(id) {
   renderHabits();
 }
 
+
+hasHabitDate = function(id, date) {
+  var habit = getHabit(id)
+
+  date = date.beginningOfDay()
+  return habit.history.any(function(d) {
+    return d.beginningOfDay().is(date)
+  })
+}
+
+toggleHabitDate = function(id, date) {
+  var habit = getHabit(id)
+
+  if (hasHabitDate(id, date)) {
+    date = date.beginningOfDay()
+    habit.history.remove(function(d) {return d.beginningOfDay().is(date)})
+  } else {
+    habit.history.push(date);
+  }
+
+  HabitDB.saveHabits()
+  renderHabits()
+}
+
+assert = function(expr) {
+  if (!expr)
+    debug.break
+}
+
 renderHabits = function() {
+  outHabits = habits.map(function(h){
+    habit = {
+      id: h.id,
+      name: h.name,
+      recentDays: []
+    }
+
+    date = Date.create()
+    for (var i=0; i<3; i++) {
+      dateEntry = {
+        id: h.id,
+        checked: hasHabitDate(h.id, date) ? "checked" : "",
+        date: date.format('{yyyy}-{MM}-{dd} {hh}:{mm}')
+      }
+
+      habit.recentDays.unshift(dateEntry);
+      date.rewind({day:1})
+    }
+
+    return habit;
+  });
+
   var tmpl = $('#habitsTmpl').html()
-  var out = Mustache.render(tmpl, {habits: habits});
+  var out = Mustache.render(tmpl, {habits: outHabits});
   $('#habits').html(out);
+
+  $('input[type="checkbox"]').change(function (e) {
+    toggleHabitDate(this.dataset.id, Date.create(this.dataset.date))
+  });
 }
 
 var HabitDB = {
@@ -37,7 +98,7 @@ var HabitDB = {
             habits = rec.values
           }
 
-          renderHabits();
+          renderHabits()
         });
       });
     }
@@ -55,7 +116,7 @@ $(function() {
     name = $('input').val()
     $('input')[0].value = ""
     event.preventDefault()
-    
+
     addHabit(name)
   });
 });
